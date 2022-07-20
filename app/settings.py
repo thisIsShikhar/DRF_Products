a87989b01c3804914e967d787bbb00339fb10075
thisIsShikhar <shikharali13@gmail.com>
2022-07-20 18:19:50 +0600

    authorization

--- app/settings.py
+++ app/settings.py
@@ -54,6 +54,13 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 2,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
 
 
