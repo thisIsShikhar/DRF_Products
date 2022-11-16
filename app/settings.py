13c472d1be42c40b0fffcfe484e9f596aa010540
thisIsShikhar <shikharali13@gmail.com>
2022-11-16 15:34:05 +0600

    changed db to postgres

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
@@ -88,10 +89,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'abcdb',
+        'USER': 'postgres',
+        'PASSWORD': 'abc',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
